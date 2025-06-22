dd7b2a432d9fd70839559d65a226f349aa1f5407
Sandeeprawat2025 <sandeeprawat15rawat@gmail.com>
2025-06-22 18:02:19 +0530

    Update sum_of_integers.py

--- sum_of_integers.py
+++ sum_of_integers.py
@@ -1,5 +1,6 @@
+
 total=0
 numbers=range(1,51)
 for a in numbers:
     total=total+a
-    print("The sum of numbers from 1 to 50 is: ",total)
+    print("The sum of numbers {numbers.step} from", numbers.start ,"to" , numbers.stop-1 , "is: ",total)
